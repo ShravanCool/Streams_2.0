c03e763b712d5501caaedf178cbb9a98a56d1dfa
Shravan <shravan.akku@gmail.com>
2021-11-13 11:10:51 +0530

    feat: Added Sign In buttons

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -20,13 +20,31 @@ class GoogleAuth extends React.Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     }
 
+    onSignInClick = () => {
+        this.auth.signIn();
+    };
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>Donno</div>;
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>Signed in</div>;
+            return (
+                <button onClick={this.onSignOutClick} className="ui red google button">
+                  <i className="google icon" />
+                  Sign Out
+                </button>
+            );
         } else {
-            return <div>Not Signed in</div>;
+            return (
+                <button onClick={this.onSignInClick} className="ui red google button">
+                  <i className="google icon" />
+                  Sign In
+                </button>
+            );
         }
     }
 
